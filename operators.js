385ec79baf56fa8d3614e98712de403aca706153
hhema20 <134532444+hhema20@users.noreply.github.com>
2023-06-02 19:08:05 +0530

    Add files via upload

---
+++ operators.js
@@ -0,0 +1,59 @@
+var a="hema";
+b="kundena";
+c= a + b;
+console.log(c);
+
+
+
+var x=6;
+y=4;
+x=x+y;
+console.log(x);
+
+x=x*3;
+console.log(x);
+x=x**2;
+console.log(x);
+
+
+
+var a=6;
+a+=8;
+console.log(a);
+a-=2;
+console.log(a);
+a*=3;
+console.log(a);
+a/=2;
+console.log(a);
+a%=3;
+console.log(a);
+
+
+/*comparison operators */
+
+x=6;
+y=3;
+console.log(x>y);
+console.log(x<y);
+console.log(x==y);
+console.log(x===y);
+
+x=8,y=2; 
+console.log(x>=y); 
+console.log(x<=y);      
+(x<10 && y>1);
+(x<10 && y<1);
+(x==0||y== 3);
+
+
+
+
+
+
+
+
+
+
+
+
